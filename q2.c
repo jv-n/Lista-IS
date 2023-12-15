a2ccfe07debc9364bae38044793c78a5c7251a4c
jv-n <jv.nascimento4@outlook.com>
2023-12-15 10:50:30 -0300

    added q2

---
+++ q2.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
+
+int main(){
+
+    int num_arq, num_threads, produtos;
+
+    scanf("%d %d %d", &num_arq, &num_threads, &produtos);
+
+    pthread_exit(NULL);
+}
